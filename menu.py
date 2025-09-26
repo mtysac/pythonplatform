19db75550d1dd21e7f95012ac0638c17bd1a0417
mtysac <mtysabelcab@gmail.com>
2025-09-26 17:50:07 -0230

    Icon added

--- menu.py
+++ menu.py
@@ -10,8 +10,8 @@ from scripts.button import Button
 
 pygame.init()
 
-#new_icon = pygame.image.load('data/images/icon.png')
-#pygame.display.set_icon(new_icon)
+new_icon = pygame.image.load('data/images/icon.png')
+pygame.display.set_icon(new_icon)
 
 SCREEN = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
 
@@ -28,7 +28,7 @@ def play():
         class Game:
 
             def __init__(self):
-                pygame.display.set_caption('memory lane')
+                pygame.display.set_caption('snoopy gameplay')
                 self.screen = SCREEN # used to be pygame.display.set_mode((1280, 720), pygame.RESIZABLE) # og coords is 640, 480
                 self.display = pygame.Surface((512, 340)) # converted
 
@@ -112,7 +112,7 @@ def options():
         pygame.display.set_caption("art")
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
-        SCREEN.blit(BG, (0, 0))
+        SCREEN.fill((0, 51, 102))#for image bg use SCREEN.blit(BG, (0, 0))
 
         #OPTIONS_TEXT = get_font(45).render("text", True, "Black")
         #OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
@@ -145,12 +145,12 @@ def main_menu():
 
     while True:
         pygame.display.set_caption("GAME!")
-        SCREEN.fill((195, 220, 255))
+        SCREEN.fill((91, 92, 94))
         #SCREEN.blit(BG, (0, 0)) was this originally
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(115).render("SNOOPY!!!!", True, "#2c60ba")
+        MENU_TEXT = get_font(115).render("SNOOPY!!!!", True, "#ffffff")
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 170))
 
         # with rect
@@ -163,11 +163,11 @@ def main_menu():
 
         # without rect
         PLAY_BUTTON = Button(image=None, pos=(245, 350), #used to be y480, switched w options
-                            text_input="START", font=get_font(60), base_color="#0e5d67", hovering_color="#a35c6a")
+                            text_input="START", font=get_font(60), base_color="#afafaf", hovering_color="#a35c6a")
         OPTIONS_BUTTON = Button(image=None, pos=(190, 480), #used to be y350
-                            text_input="ART", font=get_font(60), base_color="#0e5d67", hovering_color="#a35c6a")
+                            text_input="ART", font=get_font(60), base_color="#afafaf", hovering_color="#a35c6a")
         QUIT_BUTTON = Button(image=None, pos=(217, 600), 
-                            text_input="EXIT", font=get_font(60), base_color="#0e5d67", hovering_color="#a35c6a")
+                            text_input="EXIT", font=get_font(60), base_color="#afafaf", hovering_color="#a35c6a")
 
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
